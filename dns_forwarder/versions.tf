7d1bd0a573735b11da37318a68fb4acd7df63008
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-12-20 15:54:54 +0100

    chore: Fix version and GitHub actions (#5)
    Co-authored-by: pasqualedevita <>

--- dns_forwarder/versions.tf
+++ dns_forwarder/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.1.5"
+  required_version = ">= 1.3.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.20.0"
+      version = "<= 3.36.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = ">= 2.2.2"
+      version = "<= 2.2.3"
     }
   }
 }
--- letsencrypt_credential/versions.tf
+++ letsencrypt_credential/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.1.5"
+  required_version = ">= 1.3.0"
 
   required_providers {
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.0.0"
+      version = "<= 3.2.1"
     }
   }
 }
