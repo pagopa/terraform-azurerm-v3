d11fb7872a6bd404ce579fd8cd29763f131d5bc2
Riccardo Trivellato <6997999+rtrive@users.noreply.github.com>
2022-12-20 16:04:15 +0100

    feat: Grafana Export principal_id (#4)
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
    Co-authored-by: pasqualedevita <>

--- grafana/outputs.tf
+++ grafana/outputs.tf
@@ -7,7 +7,12 @@ output "id" {
 }
 
 output "endpoint" {
-  value = azurerm_dashboard_grafana.this.endpoint
+  description = "https endpoint"
+  value       = azurerm_dashboard_grafana.this.endpoint
+}
+
+output "hostname" {
+  value = trimprefix(azurerm_dashboard_grafana.this.endpoint, "https://")
 }
 
 output "version" {
@@ -16,4 +21,8 @@ output "version" {
 
 output "outbound_ip" {
   value = azurerm_dashboard_grafana.this.outbound_ip
+}
+
+output "principal_id" {
+  value = azurerm_dashboard_grafana.this.identity[0].principal_id
 }
