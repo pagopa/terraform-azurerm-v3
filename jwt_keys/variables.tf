3d1c375ab17fcb499b17f39be7b2dccefb9a7b4c
pasqualedevita <>
2023-02-09 10:01:22 +0100

    try fix2

--- jwt_keys/variables.tf
+++ jwt_keys/variables.tf
@@ -19,8 +19,43 @@ variable "cert_common_name" {
 }
 
 variable "cert_street_address" {
-  type = list(string)
+  type    = list(string)
   default = []
+}
+
+variable "cert_country" {
+  type    = string
+  default = ""
+}
+
+variable "cert_locality" {
+  type    = string
+  default = ""
+}
+
+variable "cert_organization" {
+  type    = string
+  default = ""
+}
+
+variable "cert_organizational_unit" {
+  type    = string
+  default = ""
+}
+
+variable "cert_postal_code" {
+  type    = string
+  default = ""
+}
+
+variable "cert_province" {
+  type    = string
+  default = ""
+}
+
+variable "cert_serial_number" {
+  type    = string
+  default = ""
 }
 
 variable "cert_password" {
