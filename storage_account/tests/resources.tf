3780f92a06c77daee0c65e3d98be73b620e300d0
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2023-03-07 13:43:29 +0100

    feat(storage_account): Add others blob features (#83)
    Co-authored-by: pasqualedevita <>

---
+++ storage_account/tests/resources.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.project}-rg"
+  location = var.location
+
+  tags = var.tags
+}
+
+module "storage_api" {
+  source = "../../storage_account"
+
+  name                            = replace("${local.project}st", "-", "")
+  account_kind                    = "StorageV2"
+  account_tier                    = "Standard"
+  access_tier                     = "Hot"
+  account_replication_type        = "GRS"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  advanced_threat_protection      = true
+  allow_nested_items_to_be_public = false
+
+  blob_versioning_enabled              = true
+  blob_container_delete_retention_days = 7
+  blob_delete_retention_days           = 7
+  blob_change_feed_enabled             = true
+  blob_change_feed_retention_in_days   = 10
+  blob_restore_policy_days             = 6
+
+  tags = var.tags
+}
--- storage_account/variables.tf
+++ storage_account/variables.tf
@@ -44,7 +44,7 @@ variable "blob_delete_retention_days" {
   default     = 0
 }
 
-variable "container_delete_retention_days" {
+variable "blob_container_delete_retention_days" {
   description = "Retention days for deleted container. Valid value is between 1 and 365 (set to 0 to disable)."
   type        = number
   default     = 0
@@ -72,6 +72,24 @@ variable "blob_versioning_enabled" {
   description = "Controls whether blob object versioning is enabled."
   type        = bool
   default     = false
+}
+
+variable "blob_change_feed_enabled" {
+  description = "(Optional) Is the blob service properties for change feed events enabled? Default to false."
+  type        = bool
+  default     = false
+}
+
+variable "blob_change_feed_retention_in_days" {
+  description = "(Optional) The duration of change feed events retention in days. The possible values are between 1 and 146000 days (400 years). Setting this to null (or omit this in the configuration file) indicates an infinite retention of the change feed."
+  type        = number
+  default     = null
+}
+
+variable "blob_restore_policy_days" {
+  description = "(Optional) Specifies the number of days that the blob can be restored, between 1 and 365 days. This must be less than the days specified for delete_retention_policy."
+  type        = number
+  default     = 0
 }
 
 variable "cross_tenant_replication_enabled" {
