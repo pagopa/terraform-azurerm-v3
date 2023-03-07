3969d4b96614c30377edb8eb9aed5431c6bc170f
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2023-03-07 14:55:49 +0100

    chore(function_app): Use standard tests structure (#84)
    Co-authored-by: pasqualedevita <>

--- function_app_slot/tests/variables.tf
+++ function_app_slot/tests/variables.tf
@@ -1,29 +1,32 @@
-variable "address_prefixes" {
-  type    = list(string)
-  default = ["10.0.1.0/26"]
-}
-
-variable "address_space" {
-  type    = list(string)
-  default = ["10.0.0.0/16"]
+variable "prefix" {
+  description = "Resorce prefix"
+  type        = string
+  default     = "azrmtest"
 }
 
 variable "location" {
-  type    = string
-  default = "westeurope"
-}
-
-variable "project" {
-  type    = string
-  default = "fnappslot"
+  description = "Resorce location"
+  type        = string
+  default     = "westeurope"
 }
 
 variable "tags" {
   type        = map(string)
-  description = "Function-app-slot example"
+  description = "Azurerm test tags"
   default = {
     CreatedBy = "Terraform"
     Source    = "https://github.com/pagopa/terraform-azurerm-v3"
   }
 }
 
+### Custom variables
+
+variable "vnet_address_space" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "function_app_subnet_cidr" {
+  type    = list(string)
+  default = ["10.0.1.0/26"]
+}
