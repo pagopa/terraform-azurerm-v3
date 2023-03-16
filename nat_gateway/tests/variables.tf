942c995f08e107fd49a567a87ca4b4361d3ef704
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2023-03-16 21:53:04 +0100

    chore(nat_gateway): Fix test (#93)
    Co-authored-by: pasqualedevita <>

--- nat_gateway/tests/variables.tf
+++ nat_gateway/tests/variables.tf
@@ -26,7 +26,12 @@ variable "vnet_address_space" {
   default = ["10.0.0.0/16"]
 }
 
-variable "subnet_cidr" {
+variable "subnet1_cidr" {
   type    = list(string)
   default = ["10.0.1.0/26"]
+}
+
+variable "subnet2_cidr" {
+  type    = list(string)
+  default = ["10.0.2.0/26"]
 }
