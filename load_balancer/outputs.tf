f439d9e80fc3d55a310ecd985234437232a6b470
ma_cloud_arch79 <matteoalongi79@gmail.com>
2023-05-08 14:03:26 +0200

    fix

--- load_balancer/outputs.tf
+++ load_balancer/outputs.tf
@@ -25,5 +25,5 @@ output "azurerm_public_ip_address" {
 
 output "azurerm_lb_backend_address_pool_id" {
   description = "the id for the azurerm_lb_backend_address_pool resource"
-  value       = azurerm_lb_backend_address_pool.this.default.id
+  value       = values(azurerm_lb_backend_address_pool.this).*.id
 }
