291e0838581ecceaf7860f1e23ac2140905b22d4
ma_cloud_arch79 <matteoalongi79@gmail.com>
2023-06-12 16:47:05 +0200

    variable remove

--- http_service_probe/variables.tf
+++ http_service_probe/variables.tf
@@ -18,16 +18,6 @@ variable "https_probe_headers" {
   description = "Https endpoint path to check"
 }
 
-variable "location_string" {
-  type        = string
-  description = "(Required) Location string"
-}
-
-variable "kv_secret_name_for_application_insights_connection_string" {
-  type        = string
-  description = "(Required) The name of the secret inside the kv that contains the application insights connection string"
-}
-
 variable "application_insights_resource_group" {
   type        = string
   description = "(Required) Application Insights resource group"
@@ -38,10 +28,10 @@ variable "application_insights_id" {
   description = "(Required) Application Insights id"
 }
 
-variable "application_insights_action_group_ids" {
-  type        = list(string)
-  description = "(Required) Application insights action group ids"
-}
+#variable "application_insights_action_group_ids" {
+#  type        = list(string)
+#  description = "(Required) Application insights action group ids"
+#}
 
 variable "alert_name" {
   type        = string
@@ -55,12 +45,3 @@ variable "alert_enabled" {
   default     = true
 }
 
-variable "keyvault_name" {
-  type        = string
-  description = "(Required) Keyvault name"
-}
-
-variable "keyvault_tenant_id" {
-  type        = string
-  description = "(Required) Keyvault tenant id"
-}
