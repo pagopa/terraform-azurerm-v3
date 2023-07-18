a36d691d0e39bfc3be57177301066f4209b91391
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-07-18 15:18:04 +0200

    mminor fix

--- kubernetes_pod_identity/main.tf
+++ kubernetes_pod_identity/main.tf
@@ -15,7 +15,8 @@ resource "azurerm_user_assigned_identity" "this" {
 }
 
 resource "azurerm_role_assignment" "managed_identity_operator" {
-  scope                = data.azurerm_kubernetes_cluster.this.id
+  # scope                = data.azurerm_kubernetes_cluster.this.id
+  scope                = data.azurerm_resource_group.aks_rg.id
   role_definition_name = "Managed Identity Operator"
   principal_id         = azurerm_user_assigned_identity.this.principal_id
 }
