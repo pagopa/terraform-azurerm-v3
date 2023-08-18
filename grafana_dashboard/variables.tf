e63cacbf4ca3c05e18e024f2ce7edd988d037bd6
ma_cloud_arch79 <matteoalongi79@gmail.com>
2023-08-18 16:47:48 +0200

    fix

--- grafana_dashboard/variables.tf
+++ grafana_dashboard/variables.tf
@@ -6,3 +6,13 @@ variable "grafana_api_key" {
   type    = string
   default = "westeurope"
 }
+
+variable "prefix" {
+  type = string
+  validation {
+    condition = (
+      length(var.prefix) <= 6
+    )
+    error_message = "Max length is 6 chars."
+  }
+}
