bdaef9c268a4a3ef96dd21ae57addcf1d0fcd342
ma_cloud_arch79 <matteoalongi79@gmail.com>
2023-08-22 16:33:23 +0200

    postgres template

--- grafana_dashboard/main.tf
+++ grafana_dashboard/main.tf
@@ -1,27 +1,27 @@
 provider "grafana" {
-   alias = "cloud"
+  alias = "cloud"
 
-   url   = var.grafana_url 
-   auth  = var.grafana_api_key 
+  url  = var.grafana_url
+  auth = var.grafana_api_key
 }
 
 locals {
-allowed_resource_by_file=fileset("${path.module}/dashboard","*.json")
-allowed_resource_type_replaced = [
-        for item in local.allowed_resource_by_file:
-                replace(item, "_", "/")
-        ]
-allowed_resource_type =  [
-        for item in local.allowed_resource_type_replaced:
-                trimsuffix(item,".json")
-        ]
+  allowed_resource_by_file = fileset("${path.module}/dashboard", "*.json")
+  allowed_resource_type_replaced = [
+    for item in local.allowed_resource_by_file :
+    replace(item, "_", "/")
+  ]
+  allowed_resource_type = [
+    for item in local.allowed_resource_type_replaced :
+    trimsuffix(item, ".json")
+  ]
 
 
 }
 
 data "azurerm_resources" "sub_resources" {
   for_each = toset(local.allowed_resource_type)
-  type = each.key
+  type     = each.key
 
   required_tags = {
     grafana = "yes"
@@ -29,37 +29,38 @@ data "azurerm_resources" "sub_resources" {
 }
 
 locals {
-dashboard_folder_map =  flatten([
+  dashboard_folder_map = flatten([
     for rt in data.azurerm_resources.sub_resources : {
-        name        = split("/",rt.type)[1]
+      name = split("/", rt.type)[1]
     }
   ])
 
-dashboard_resource_map = flatten([
+  dashboard_resource_map = flatten([
     for rt in data.azurerm_resources.sub_resources : [
       for d in rt.resources : {
-        type          = d.type
-        name          = d.name
-        rgroup        = split("/",d.id)[4]
+        type   = d.type
+        name   = d.name
+        rgroup = split("/", d.id)[4]
+        sub = split("/", d.id)[0]
       }
     ]
   ])
 
-  
+
 }
 
 resource "grafana_folder" "domainsfolder" {
-   provider = grafana.cloud
-   for_each    = { for i in range(length(local.dashboard_folder_map)) : local.dashboard_folder_map[i].name => i }
+  provider = grafana.cloud
+  for_each = { for i in range(length(local.dashboard_folder_map)) : local.dashboard_folder_map[i].name => i }
 
-   title = "${upper(var.prefix)}-${upper(local.dashboard_folder_map[each.value].name)}-dashboard"
+  title = "${upper(var.prefix)}-${upper(local.dashboard_folder_map[each.value].name)}-dashboard"
 }
 
 resource "grafana_dashboard" "azure_monitor_storage_insights" {
-  provider    = grafana.cloud
-  for_each    = { for i in range(length(local.dashboard_resource_map)) : local.dashboard_resource_map[i].name => i }
+  provider = grafana.cloud
+  for_each = { for i in range(length(local.dashboard_resource_map)) : local.dashboard_resource_map[i].name => i }
 
-  config_json = templatefile("${path.module}/dashboard/${replace(local.dashboard_resource_map[each.value].type,"/","_")}.json", { TITOLOTEST = "${format("%s",local.dashboard_resource_map[each.value].name)}"})
-  folder = grafana_folder.domainsfolder["${split("/",local.dashboard_resource_map[each.value].type)[1]}"].id
-  overwrite = true 
+  config_json = templatefile("${path.module}/dashboard/${replace(local.dashboard_resource_map[each.value].type, "/", "_")}.json", { resource = "${format("%s", local.dashboard_resource_map[each.value].name)}", rg = "${format("%s", local.dashboard_resource_map[each.value].rgroup)}", sub = "${format("%s", local.dashboard_resource_map[each.value].sub)}" })
+  folder      = grafana_folder.domainsfolder["${split("/", local.dashboard_resource_map[each.value].type)[1]}"].id
+  overwrite   = true
 }
