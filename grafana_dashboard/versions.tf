a000757d4fa02bfb1418ab70b09b758569b4942e
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2023-09-07 18:35:07 +0200

    feat: Upgrade to azurerm 3.71.0 (#156)
    Co-authored-by: pasqualedevita <>

--- grafana_dashboard/versions.tf
+++ grafana_dashboard/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "<= 3.64.0"
+      version = ">= 3.30.0, <= 3.71.0"
     }
     grafana = {
       source  = "grafana/grafana"
-      version = ">= 2.2.0"
+      version = "<= 2.3.0"
     }
   }
 }
