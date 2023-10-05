8719274ab42f78bbb3149c6fad86f2d2594e8981
Andrea Grillo <andrea.grillo@pagopa.it>
2023-10-05 17:01:26 +0200

    resolve outputs

--- identity_github_runner/outputs.tf
+++ identity_github_runner/outputs.tf
@@ -1,8 +1,19 @@
-output "user_managed_identity" {
-  value = {
-    resource_group_name = azurerm_user_assigned_identity.identity.resource_group_name
-    app_name            = azurerm_user_assigned_identity.identity.name
-    client_id           = azurerm_user_assigned_identity.identity.client_id
-    object_id           = azurerm_user_assigned_identity.identity.principal_id
-  }
+output "identity_resource_group" {
+  value       = azurerm_user_assigned_identity.identity.resource_group_name
+  description = "User Managed Identity resource group"
+}
+
+output "identity_app_name" {
+  value       = azurerm_user_assigned_identity.identity.name
+  description = "User Managed Identity name"
+}
+
+output "identity_client_id" {
+  value       = azurerm_user_assigned_identity.identity.client_id
+  description = "User Managed Identity client id"
+}
+
+output "identity_principal_id" {
+  value       = azurerm_user_assigned_identity.identity.principal_id
+  description = "User Managed Identity principal id"
 }
