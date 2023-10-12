da10c6a049846b9a231df945be6b3126e9d7bd15
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-10-12 14:18:09 +0200

    init module

---
+++ dns_forwarder_vm_image/main.tf
@@ -0,0 +1,113 @@
+locals {
+  target_image_name = "${var.image_name}-${var.image_version}"
+  target_image_id   = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Compute/images/${local.target_image_name}"
+}
+
+data "azuread_client_config" "current" {}
+
+data "azurerm_resource_group" "target_resource_group" {
+  name = var.resource_group_name
+}
+
+resource "random_id" "rg_randomizer" {
+  keepers = {
+    image_name    = var.image_name
+    image_version = var.image_version
+  }
+  byte_length = 8
+}
+
+resource "azuread_application" "packer_application" {
+  display_name = "${var.prefix}-packer-application"
+  owners       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_application_password" "velero_application_password" {
+  application_object_id = azuread_application.packer_application.object_id
+}
+
+resource "azuread_service_principal" "packer_sp" {
+
+  application_id = azuread_application.packer_application.application_id
+  owners         = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal_password" "packer_principal_password" {
+  service_principal_id = azuread_service_principal.packer_sp.object_id
+}
+
+resource "azurerm_role_assignment" "packer_sp_sub_reader_role" {
+  scope                = "/subscriptions/${var.subscription_id}"
+  role_definition_name = "Reader"
+  principal_id         = azuread_service_principal.packer_sp.object_id
+}
+
+
+resource "azurerm_role_assignment" "packer_sp_rg_role" {
+  scope                = data.azurerm_resource_group.target_resource_group.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.packer_sp.object_id
+}
+
+resource "azurerm_resource_group" "build_rg" {
+  location = var.location
+  name     = "${var.build_rg_name}-${random_id.rg_randomizer.hex}"
+}
+
+resource "azurerm_role_assignment" "packer_sp_build_rg_role" {
+  scope                = azurerm_resource_group.build_rg.id
+  role_definition_name = "Owner"
+  principal_id         = azuread_service_principal.packer_sp.object_id
+}
+
+
+resource "null_resource" "build_packer_image" {
+
+  triggers = {
+    subscription               = var.subscription_id
+    target_resource_group_name = var.resource_group_name
+    base_image_publisher       = var.base_image_publisher
+    base_image_offer           = var.base_image_offer
+    base_image_sku             = var.base_image_sku
+    base_image_version         = var.base_image_version
+    vm_sku                     = var.vm_sku
+    target_image_name          = local.target_image_name
+    location                   = var.location
+  }
+
+  depends_on = [
+    azurerm_role_assignment.packer_sp_build_rg_role,
+    azurerm_resource_group.build_rg,
+    azuread_application.packer_application
+  ]
+
+  # remove old packer cache
+  provisioner "local-exec" {
+    command = <<EOT
+    rm -rf "$HOME/.azure/packer"
+    EOT
+  }
+
+  # requires packer https://developer.hashicorp.com/packer/tutorials/docker-get-started/get-started-install-cli
+  provisioner "local-exec" {
+    working_dir = "${path.module}/packer"
+    command     = <<EOT
+    packer init . && \
+    packer build \
+    -var "subscription=${var.subscription_id}" \
+    -var "target_resource_group_name=${var.resource_group_name}" \
+    -var "base_image_publisher=${var.base_image_publisher}" \
+    -var "base_image_offer=${var.base_image_offer}" \
+    -var "base_image_sku=${var.base_image_sku}" \
+    -var "base_image_version=${var.base_image_version}" \
+    -var "vm_sku=${var.vm_sku}" \
+    -var "target_image_name=${local.target_image_name}" \
+    -var "location=${var.location}" \
+    -var "client_id=${azuread_application.packer_application.application_id}" \
+    -var "client_secret=${azuread_application_password.velero_application_password.value}" \
+    -var "build_rg_name=${azurerm_resource_group.build_rg.name}" \
+    .
+    EOT
+  }
+
+}
---
+++ dns_forwarder_vm_image/variables.tf
@@ -0,0 +1,75 @@
+variable "location" {
+  type        = string
+  description = "(Required) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "(Required) Azure subscription id"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "(Required) The name of the Resource Group in which the custom image will be created"
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine_scale_set#source_image_reference
+variable "base_image_publisher" {
+  type        = string
+  default     = "Canonical"
+  description = "(Optional) - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine_scale_set#source_image_reference"
+}
+
+variable "image_name" {
+  type        = string
+  description = "(Required) name assigned to the generated image. Note that the pair <image_name, image_version> must be unique and not already existing"
+}
+
+variable "image_version" {
+  type        = string
+  description = "(Required) Version assigned to the generated image. Note that the pair <image_name, image_version> must be unique and not already existing"
+}
+
+variable "force_replacement" {
+  type        = bool
+  description = "(Optional) Wheather if the image should be deleted and recreated even if already existing"
+  default     = false
+}
+
+variable "base_image_offer" {
+  type        = string
+  default     = "0001-com-ubuntu-server-jammy"
+  description = "(Optional) - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine_scale_set#source_image_reference"
+}
+variable "base_image_sku" {
+  type        = string
+  default     = "22_04-lts-gen2"
+  description = "(Optional) - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine_scale_set#source_image_reference"
+}
+
+variable "base_image_version" {
+  type        = string
+  default     = "latest"
+  description = "(Optional) - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine_scale_set#source_image_reference"
+}
+
+variable "vm_sku" {
+  type        = string
+  description = "(Optional) Size of VMs in the scale set. Default to Standard_B1s. See https://azure.microsoft.com/pricing/details/virtual-machines/ for size info."
+  default     = "Standard_B1s"
+}
+
+variable "tags" {
+  type = map(any)
+}
+
+variable "prefix" {
+  type        = string
+  description = "(Required) prefix used in resource creation"
+}
+
+variable "build_rg_name" {
+  type        = string
+  description = "(Optional) Packer build temporary resource group name"
+  default     = "tmp-packer-build"
+}
