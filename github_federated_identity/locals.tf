6faeba42d83218bbb329cd976fcca95a42511c8f
Andrea Grillo <andrea.grillo@pagopa.it>
2023-10-26 12:33:28 +0200

    Update naming convention

--- github_federated_identity/locals.tf
+++ github_federated_identity/locals.tf
@@ -2,6 +2,6 @@ locals {
   name = var.domain == "" ? "${var.prefix}-${var.env_short}" : "${var.prefix}-${var.env_short}-${var.domain}"
 
   resource_group_name = "${local.name}-identity-rg"
-  identity_name       = "${local.name}-github-identity-${var.identity_role}"
+  identity_name       = "${local.name}-github-${var.identity_role}-identity"
   federation_prefix   = "${local.name}-github"
 }
