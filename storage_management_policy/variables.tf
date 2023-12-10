98389a0ccd225e64dc53f608c20005a53d39e631
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-12-10 12:37:05 +0100

    added to policy the snapshot and version fields

--- storage_management_policy/variables.tf
+++ storage_management_policy/variables.tf
@@ -8,18 +8,34 @@ variable "rules" {
     name    = string
     enabled = bool
     filters = object({
-      prefix_match = list(string)
-      blob_types   = list(string)
+      prefix_match = list(string) # (Optional) An array of strings for prefixes to be matched.
+      blob_types   = list(string) # (Required) An array of predefined values. Valid options are blockBlob and appendBlob.
     })
     actions = object({
-      snapshot = object({
-        delete_after_days_since_creation_greater_than = number
-      })
       base_blob = object({
-        tier_to_cool_after_days_since_modification_greater_than    = number
-        tier_to_archive_after_days_since_modification_greater_than = number
-        delete_after_days_since_modification_greater_than          = number
+        delete_after_days_since_modification_greater_than              = number
+        delete_after_days_since_creation_greater_than                  = optional(number, -1)
+        delete_after_days_since_last_access_time_greater_than          = optional(number, -1)
+        tier_to_cool_after_days_since_modification_greater_than        = number
+        tier_to_cool_after_days_since_creation_greater_than            = optional(number, -1)
+        tier_to_cool_after_days_since_last_access_time_greater_than    = optional(number, -1)
+        tier_to_archive_after_days_since_modification_greater_than     = number
+        tier_to_archive_after_days_since_creation_greater_than         = optional(number, -1)
+        tier_to_archive_after_days_since_last_access_time_greater_than = optional(number, -1)
+        tier_to_archive_after_days_since_last_tier_change_greater_than = optional(number, -1)
       })
+      snapshot = optional(object({
+        change_tier_to_archive_after_days_since_creation               = number
+        change_tier_to_cool_after_days_since_creation                  = number
+        delete_after_days_since_creation_greater_than                  = optional(number, -1)
+        tier_to_archive_after_days_since_last_tier_change_greater_than = optional(number, -1)
+      }), null)
+      version = optional(object({
+        change_tier_to_archive_after_days_since_creation               = number
+        change_tier_to_cool_after_days_since_creation                  = number
+        delete_after_days_since_creation                               = optional(number, -1)
+        tier_to_archive_after_days_since_last_tier_change_greater_than = optional(number, -1)
+      }), null)
     })
   }))
   default = []
