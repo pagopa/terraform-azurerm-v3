ed75eb8391d812492d28d898b54381c913f43d6c
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-12-12 14:42:32 +0100

    removed format when possible

--- eventhub/main.tf
+++ eventhub/main.tf
@@ -3,13 +3,13 @@ locals {
     [for c in h.consumers : {
       hub  = h.name
       name = c
-  }]]) : format("%s.%s", hc.hub, hc.name) => hc }
+  }]]) : "${hc.hub}.${hc.name}" => hc }
 
   keys = { for hk in flatten([for h in var.eventhubs :
     [for k in h.keys : {
       hub = h.name
       key = k
-  }]]) : format("%s.%s", hk.hub, hk.key.name) => hk }
+  }]]) : "${hk.hub}.${hk.key.name}" => hk }
 
   hubs = { for h in var.eventhubs : h.name => h }
 }
@@ -111,19 +111,19 @@ resource "azurerm_private_dns_zone_virtual_network_link" "eventhub" {
 resource "azurerm_private_endpoint" "eventhub" {
   count = var.sku != "Basic" ? 1 : 0
 
-  name                = format("%s-private-endpoint", var.name)
+  name                = "${var.name}-private-endpoint"
   location            = var.location
   resource_group_name = var.resource_group_name
   subnet_id           = var.subnet_id
 
   private_dns_zone_group {
-    name = format("%s-private-dns-zone-group", var.name)
+    name = "${var.name}-private-dns-zone-group"
     # One of the concatenated arrays is empty
     private_dns_zone_ids = concat(var.private_dns_zones.id, azurerm_private_dns_zone.eventhub.*.id)
   }
 
   private_service_connection {
-    name                           = format("%s-private-service-connection", var.name)
+    name                           = "${var.name}-private-service-connection"
     private_connection_resource_id = azurerm_eventhub_namespace.this.id
     is_manual_connection           = false
     subresource_names              = ["namespace"]
