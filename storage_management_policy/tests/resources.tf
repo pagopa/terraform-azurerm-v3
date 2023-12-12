d1d6cbaba0c9ad038d57d41e4d1bb3f1553d41c9
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-12-12 10:52:53 +0100

    updated test example

--- storage_management_policy/tests/resources.tf
+++ storage_management_policy/tests/resources.tf
@@ -26,55 +26,47 @@ module "storage_account" {
   tags = var.tags
 }
 
-module "storage_account_management_policy_complete" {
+module "storage_account_management_policy_example" {
   source = "../../storage_management_policy"
 
   storage_account_id = module.storage_account.id
 
   rules = [{
-    name    = "${local.project}policysapolicy-complete"
+    name    = "${local.project}policysapolicy-blockBlob"
     enabled = true
     filters = {
-      prefix_match = ["container2/prefix1", "container2/prefix2"]
+      prefix_match = ["files-to-archive", "container2/prefix2"]
       blob_types   = ["blockBlob"]
     }
     actions = {
       base_blob = {
-        tier_to_cool_after_days_since_modification_greater_than    = 1
-        tier_to_archive_after_days_since_modification_greater_than = 2
-        delete_after_days_since_modification_greater_than          = 10
+        tier_to_cool_after_days_since_modification_greater_than    = 0
+        tier_to_archive_after_days_since_modification_greater_than = 1
+        # delete_after_days_since_modification_greater_than          = 10
       }
       snapshot = {
-        change_tier_to_archive_after_days_since_creation = 1
-        change_tier_to_cool_after_days_since_creation    = 2
+        change_tier_to_archive_after_days_since_creation = 0
+        change_tier_to_cool_after_days_since_creation    = 1
         delete_after_days_since_creation_greater_than    = 20
       }
       version = {
-        change_tier_to_archive_after_days_since_creation = 1
-        change_tier_to_cool_after_days_since_creation    = 2
+        change_tier_to_archive_after_days_since_creation = 0
+        change_tier_to_cool_after_days_since_creation    = 1
         delete_after_days_since_creation                 = 30
       }
     }
-  }]
-}
-
-module "storage_account_management_policy_base" {
-  source = "../../storage_management_policy"
-
-  storage_account_id = module.storage_account.id
-
-  rules = [{
-    name    = "${local.project}policysapolicy-base"
+  },
+  # appendBlob only support delete policy
+  {
+    name    = "${local.project}policysapolicy-appendBlob"
     enabled = true
     filters = {
       prefix_match = ["insights-logs-auditevent"]
-      blob_types   = ["blockBlob"]
+      blob_types   = ["appendBlob"]
     }
     actions = {
       base_blob = {
-        tier_to_cool_after_days_since_modification_greater_than    = 1
-        tier_to_archive_after_days_since_modification_greater_than = 2
-        delete_after_days_since_modification_greater_than          = 10
+        delete_after_days_since_modification_greater_than          = 1
       }
     }
   }]
