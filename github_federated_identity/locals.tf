e564a7d038965a73a79a790c996c5bfb55c8b908
Andrea Grillo <andrea.grillo@pagopa.it>
2023-12-19 11:36:34 +0100

    fmt

--- github_federated_identity/locals.tf
+++ github_federated_identity/locals.tf
@@ -3,6 +3,5 @@ locals {
 
   resource_group_name = "${var.prefix}-${var.env_short}-identity-rg"
   identity_name       = var.app_name == "" ? "${local.name}-github-${var.identity_role}-identity" : "${local.name}-${var.app_name}-github-${var.identity_role}-identity"
-  # federation_prefix   = var.app_name == "" ? "${local.name}-github" : "${local.name}-${var.app_name}-github"
-  federation_prefix = "${local.name}-github"
+  federation_prefix   = "${local.name}-github"
 }
