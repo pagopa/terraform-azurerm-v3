dc97f03572a0224dbb212bbcbbeffb847c11241e
Andrea Grillo <andrea.grillo@pagopa.it>
2023-12-19 11:32:42 +0100

    playing with app_name var

--- github_federated_identity/locals.tf
+++ github_federated_identity/locals.tf
@@ -2,6 +2,7 @@ locals {
   name = var.domain == "" ? "${var.prefix}-${var.env_short}" : "${var.prefix}-${var.env_short}-${var.domain}"
 
   resource_group_name = "${var.prefix}-${var.env_short}-identity-rg"
-  identity_name       = "${local.name}-github-${var.identity_role}-identity"
-  federation_prefix   = var.app_name == "" ? "${local.name}-github" : "${local.name}-${var.app_name}-github"
+  identity_name       = var.app_name == "" ? "${local.name}-github-${var.identity_role}-identity" : "${local.name}-${var.app_name}-github-${var.identity_role}-identity"
+  # federation_prefix   = var.app_name == "" ? "${local.name}-github" : "${local.name}-${var.app_name}-github"
+  federation_prefix = "${local.name}-github"
 }
