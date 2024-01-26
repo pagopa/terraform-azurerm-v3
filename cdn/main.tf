f5c1ceb5113ba72ba037fe5ae557f686d6fd252c
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-26 14:21:51 +0100

    upgrated internals modules

--- cdn/main.tf
+++ cdn/main.tf
@@ -3,7 +3,7 @@
  **/
 module "cdn_storage_account" {
 
-  source = "github.com/pagopa/terraform-azurerm-v3.git//storage_account?ref=v7.44.0"
+  source = "github.com/pagopa/terraform-azurerm-v3.git//storage_account?ref=v7.47.2"
 
   name = replace("${var.prefix}-${var.name}-sa", "-", "")
 
