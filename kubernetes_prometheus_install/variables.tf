84a7bffd3ef102534002ae5aee6b3bf2af24f259
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-03-07 11:23:59 +0100

    pre-commit fixs

--- kubernetes_prometheus_install/variables.tf
+++ kubernetes_prometheus_install/variables.tf
@@ -11,7 +11,7 @@ variable "storage_class_name" {
 
 variable "prometheus_helm" {
   type = object({
-    chart_version = optional(string,"25.16.0")
+    chart_version = optional(string, "25.16.0")
     alertmanager = object({
       image_name = optional(string, "quay.io/prometheus/alertmanager"),
       image_tag  = optional(string, "v0.27.0"),
@@ -38,31 +38,31 @@ variable "prometheus_helm" {
     }),
   })
   description = "Prometheus helm chart configuration"
-#  default = {
-#    chart_version = "25.16.0"
-#    alertmanager = {
-#      image_name = "quay.io/prometheus/alertmanager"
-#      image_tag  = "v0.27.0",
-#    }
-#    configmap_reload_prometheus = {
-#      image_name = "jimmidyson/configmap-reload"
-#      image_tag  = "v0.12.0"
-#    }
-#    configmap_reload_alertmanager = {
-#      image_name = "jimmidyson/configmap-reload"
-#      image_tag  = "v0.12.0"
-#    }
-#    node_exporter = {
-#      image_name = "quay.io/prometheus/node-exporter"
-#      image_tag  = "v1.7.0"
-#    }
-#    server = {
-#      image_name = "quay.io/prometheus/prometheus"
-#      image_tag  = "v2.50.1",
-#    }
-#    pushgateway = {
-#      image_name = "prom/pushgateway"
-#      image_tag  = "v1.7.0"
-#    }
-#  }
+  #  default = {
+  #    chart_version = "25.16.0"
+  #    alertmanager = {
+  #      image_name = "quay.io/prometheus/alertmanager"
+  #      image_tag  = "v0.27.0",
+  #    }
+  #    configmap_reload_prometheus = {
+  #      image_name = "jimmidyson/configmap-reload"
+  #      image_tag  = "v0.12.0"
+  #    }
+  #    configmap_reload_alertmanager = {
+  #      image_name = "jimmidyson/configmap-reload"
+  #      image_tag  = "v0.12.0"
+  #    }
+  #    node_exporter = {
+  #      image_name = "quay.io/prometheus/node-exporter"
+  #      image_tag  = "v1.7.0"
+  #    }
+  #    server = {
+  #      image_name = "quay.io/prometheus/prometheus"
+  #      image_tag  = "v2.50.1",
+  #    }
+  #    pushgateway = {
+  #      image_name = "prom/pushgateway"
+  #      image_tag  = "v1.7.0"
+  #    }
+  #  }
 }
