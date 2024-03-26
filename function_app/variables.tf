d31f3cb34e9471071911b65cbceb4f9a281ed8fd
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-26 14:12:14 +0100

    feat: Add storage account variables to function app module (#257)

--- function_app/variables.tf
+++ function_app/variables.tf
@@ -49,6 +49,7 @@ variable "storage_account_info" {
     access_tier                       = string # Defines the access tier for BlobStorage, FileStorage and StorageV2 accounts. Valid options are Hot and Cool, defaults to Hot.
     advanced_threat_protection_enable = bool
     use_legacy_defender_version       = bool
+    public_network_access_enabled     = bool
   })
 
   default = {
@@ -58,6 +59,7 @@ variable "storage_account_info" {
     access_tier                       = "Hot"
     advanced_threat_protection_enable = true
     use_legacy_defender_version       = true
+    public_network_access_enabled     = false
   }
 }
 
@@ -68,6 +70,8 @@ variable "internal_storage_account_info" {
     account_replication_type          = string # Defines the type of replication to use for this storage account. Valid options are LRS, GRS, RAGRS, ZRS, GZRS and RAGZRS.
     access_tier                       = string # Defines the access tier for BlobStorage, FileStorage and StorageV2 accounts. Valid options are Hot and Cool, defaults to Hot.
     advanced_threat_protection_enable = bool
+    use_legacy_defender_version       = bool
+    public_network_access_enabled     = bool
   })
 
   default = null
