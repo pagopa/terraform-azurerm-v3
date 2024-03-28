131bd40fe7b3cac3778340d93ed2bc8b47bee40a
Andrea Grillo <andrea.grillo@pagopa.it>
2024-03-28 15:57:13 +0100

    fix outputs

--- container_app_environment_workload_profile/outputs.tf
+++ container_app_environment_workload_profile/outputs.tf
@@ -3,9 +3,9 @@ output "id" {
 }
 
 output "resource_group_name" {
-  value = azurerm_container_app_environment.container_app_environment.parent_id
+  value = azapi_resource.container_app_environment.parent_id
 }
 
 output "name" {
-  value = azurerm_container_app_environment.container_app_environment.name
+  value = azapi_resource.container_app_environment.name
 }
