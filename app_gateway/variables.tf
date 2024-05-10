21fbf433cfbe903c5684b28e1bda8cac9703a441
christian-calabrese <christian.calabrese@pagopa.it>
2024-05-10 09:27:10 +0200

    fix: reroute is now an optional field in app_gateway

--- app_gateway/variables.tf
+++ app_gateway/variables.tf
@@ -153,9 +153,9 @@ variable "rewrite_rule_sets" {
       }))
 
       url = object({
-        path         = string # The URL path to rewrite.
-        query_string = string # The query string to rewrite.
-        reroute      = bool   # Whether the URL path map should be reevaluated after this rewrite has been applied.
+        path         = string                # The URL path to rewrite.
+        query_string = string                # The query string to rewrite.
+        reroute      = optional(bool, false) # Whether the URL path map should be reevaluated after this rewrite has been applied.
       })
 
     }))
