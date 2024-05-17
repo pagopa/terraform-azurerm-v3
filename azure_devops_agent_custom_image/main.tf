3b7fc0506380ded1e6c5e8777dd5c593f2661718
mamari90 <130982006+mamari90@users.noreply.github.com>
2024-05-17 17:08:38 +0200

    test

--- azure_devops_agent_custom_image/main.tf
+++ azure_devops_agent_custom_image/main.tf
@@ -62,8 +62,7 @@ resource "null_resource" "build_packer_image" {
   provisioner "local-exec" {
     working_dir = "${path.module}/packer"
     command     = <<EOT
-      packer init .
-
+      packer init . ; \
       packer build \
       -var "target_resource_group_name=${var.resource_group_name}" \
       -var "base_image_publisher=${var.base_image_publisher}" \
