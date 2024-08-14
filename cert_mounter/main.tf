332c371bbb7a10e620b01444defee96ea3740668
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-08-14 16:58:10 +0200

    added configuration for workload identity

--- cert_mounter/main.tf
+++ cert_mounter/main.tf
@@ -2,18 +2,26 @@ resource "helm_release" "cert_mounter" {
   name         = "cert-mounter-blueprint"
   repository   = "https://pagopa.github.io/aks-helm-cert-mounter-blueprint"
   chart        = "cert-mounter-blueprint"
-  version      = "1.0.4"
+  version      = local.chart_version
   namespace    = var.namespace
   timeout      = 120
   force_update = true
 
   values = [
-    templatefile("${path.module}/helm/cert-mounter-yaml.tpl", {
-      NAMESPACE        = var.namespace,
-      CERTIFICATE_NAME = var.certificate_name,
-      KEY_VAULT_NAME   = var.kv_name
-      TENANT_ID        = var.tenant_id
-    })
-
+    var.workload_identity_enabled ?
+      templatefile("cert-mounter-workload-identity.yaml.tpl", {
+        NAMESPACE        = var.namespace,
+        CERTIFICATE_NAME = var.certificate_name,
+        KEY_VAULT_NAME   = var.kv_name
+        TENANT_ID        = var.tenant_id
+        SERVICE_ACCOUNT_NAME = var.workload_identity_service_account_name
+        WORKLOAD_IDENTITY_CLIENT_ID = var.workload_identity_client_id
+      }) :
+      templatefile("cert-mounter-pod-identity.yaml.tpl", {
+        NAMESPACE        = var.namespace,
+        CERTIFICATE_NAME = var.certificate_name,
+        KEY_VAULT_NAME   = var.kv_name
+        TENANT_ID        = var.tenant_id
+      })
   ]
 }
--- cert_mounter/variables.tf
+++ cert_mounter/variables.tf
@@ -1,3 +1,7 @@
+locals {
+  chart_version = var.workload_identity_enabled ? "2.0.0" : "1.0.4"
+}
+
 variable "namespace" {
   type        = string
   description = "(Required) Namespace where the cert secret will be created"
@@ -17,4 +21,27 @@ variable "tenant_id" {
   type        = string
   description = "(Required) Tenant identifier"
 }
+
+variable "cert_mounter_chart_version" {
+  type = string
+  description = "(Optional) Cert mounter chart version"
+  default = "1.0.4"
+}
+
+variable "workload_identity_enabled" {
+  type = bool
+  description = "Enable workload identity chart"
+  default = false
+}
+
+variable "workload_identity_service_account_name" {
+  type = string
+  description = "Service account name linked to workload identity"
+}
+
+variable "workload_identity_client_id" {
+  type = string
+  description = "ClientID in form of 'qwerty123-a1aa-1234-xyza-qwerty123' linked to workload identity"
+}
+
 
