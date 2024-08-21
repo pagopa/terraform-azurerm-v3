843d65aa3b1197d9f9bdf5e5f37274d31e28afa3
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-08-21 14:00:08 +0200

    pre-commit fixs

--- tls_checker/main.tf
+++ tls_checker/main.tf
@@ -2,11 +2,11 @@ resource "helm_release" "helm_this" {
 
   count = var.helm_chart_present ? 1 : 0
 
-  namespace  = var.namespace
-  name       = local.helm_chart_name
-  chart      = "microservice-chart"
-  repository = "https://pagopa.github.io/aks-microservice-chart-blueprint"
-  version    = local.chart_version
+  namespace    = var.namespace
+  name         = local.helm_chart_name
+  chart        = "microservice-chart"
+  repository   = "https://pagopa.github.io/aks-microservice-chart-blueprint"
+  version      = local.chart_version
   timeout      = 120
   force_update = true
 
@@ -26,24 +26,24 @@ resource "helm_release" "helm_this" {
         keyvault_name                                             = var.keyvault_name
         keyvault_tenant_id                                        = var.keyvault_tenant_id
         kv_secret_name_for_application_insights_connection_string = var.kv_secret_name_for_application_insights_connection_string
-        SERVICE_ACCOUNT_NAME = var.workload_identity_service_account_name
-        WORKLOAD_IDENTITY_CLIENT_ID = var.workload_identity_client_id
-    }):
+        SERVICE_ACCOUNT_NAME                                      = var.workload_identity_service_account_name
+        WORKLOAD_IDENTITY_CLIENT_ID                               = var.workload_identity_client_id
+    }) :
     templatefile("${path.module}/templates/tls-cert.yaml.tpl",
-    {
-      namespace                                                 = var.namespace
-      image_name                                                = var.helm_chart_image_name
-      image_tag                                                 = var.helm_chart_image_tag
-      website_site_name                                         = var.https_endpoint
-      time_trigger                                              = var.time_trigger
-      function_name                                             = var.https_endpoint
-      region                                                    = var.location_string
-      expiration_delta_in_days                                  = var.expiration_delta_in_days
-      host                                                      = var.https_endpoint
-      keyvault_name                                             = var.keyvault_name
-      keyvault_tenant_id                                        = var.keyvault_tenant_id
-      kv_secret_name_for_application_insights_connection_string = var.kv_secret_name_for_application_insights_connection_string
-  })
+      {
+        namespace                                                 = var.namespace
+        image_name                                                = var.helm_chart_image_name
+        image_tag                                                 = var.helm_chart_image_tag
+        website_site_name                                         = var.https_endpoint
+        time_trigger                                              = var.time_trigger
+        function_name                                             = var.https_endpoint
+        region                                                    = var.location_string
+        expiration_delta_in_days                                  = var.expiration_delta_in_days
+        host                                                      = var.https_endpoint
+        keyvault_name                                             = var.keyvault_name
+        keyvault_tenant_id                                        = var.keyvault_tenant_id
+        kv_secret_name_for_application_insights_connection_string = var.kv_secret_name_for_application_insights_connection_string
+    })
   ]
 }
 
