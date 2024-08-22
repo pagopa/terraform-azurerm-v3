9852d178256fb933f42f63912068df3b608993db
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-08-22 12:16:31 +0200

    fix version for aks

--- kubernetes_workload_identity/versions.tf
+++ kubernetes_workload_identity/versions.tf
@@ -1,14 +1,18 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.30"
+      version = "~>3.110"
     }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30.0"
     }
   }
 }
