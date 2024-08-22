b77e864424fb2916a75a9782d6bacc93292cd767
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-08-22 12:16:48 +0200

    fix outputs

--- kubernetes_workload_identity/outputs.tf
+++ kubernetes_workload_identity/outputs.tf
@@ -6,14 +6,18 @@ output "user_assigned_identity_client_id" {
   value = azurerm_user_assigned_identity.this.client_id
 }
 
+output "workload_identity_client_id" {
+  value = azurerm_user_assigned_identity.this.client_id
+}
+
 output "workload_identity_service_account_name" {
-  value = kubernetes_service_account_v1.workload_identity_sa[0].metadata.name
+  value = try(kubernetes_service_account_v1.workload_identity_sa[0].metadata[0].name, null)
 }
 
 output "workload_identity_service_account_namespace" {
-  value = kubernetes_service_account_v1.workload_identity_sa[0].metadata.namespace
+  value = try(kubernetes_service_account_v1.workload_identity_sa[0].metadata[0].namespace, null)
 }
 
 output "workload_identity_client_id_secret_name" {
-  value = azurerm_key_vault_secret.workload_identity_client_id.name
+  value = try(azurerm_key_vault_secret.workload_identity_client_id[0].name, null)
 }
