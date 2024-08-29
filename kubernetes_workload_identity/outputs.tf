09521b30a15b9e9f15d09aae97d93acb6b549d9c
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-08-29 16:44:36 +0200

    added more outputs for workload identity

--- kubernetes_workload_identity/outputs.tf
+++ kubernetes_workload_identity/outputs.tf
@@ -2,6 +2,14 @@ output "user_assigned_identity_id" {
   value = azurerm_user_assigned_identity.this.id
 }
 
+output "user_assigned_identity_name" {
+  value = azurerm_user_assigned_identity.this.name
+}
+
+output "user_assigned_identity_resource_group_name" {
+  value = azurerm_user_assigned_identity.this.resource_group_name
+}
+
 output "user_assigned_identity_client_id" {
   value = azurerm_user_assigned_identity.this.client_id
 }
