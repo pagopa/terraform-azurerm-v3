67462124a8dd7b9f6062d1a10ef68312fbeba753
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-09-02 15:09:02 +0200

    fix outputs

--- kubernetes_workload_identity_configuration/outputs.tf
+++ kubernetes_workload_identity_configuration/outputs.tf
@@ -1,29 +1,29 @@
 output "user_assigned_identity_id" {
-  value = azurerm_user_assigned_identity.this.id
+  value = data.azurerm_user_assigned_identity.this.id
 }
 
 output "user_assigned_identity_name" {
-  value = azurerm_user_assigned_identity.this.name
+  value = data.azurerm_user_assigned_identity.this.name
 }
 
 output "user_assigned_identity_resource_group_name" {
-  value = azurerm_user_assigned_identity.this.resource_group_name
+  value = data.azurerm_user_assigned_identity.this.resource_group_name
 }
 
 output "user_assigned_identity_client_id" {
-  value = azurerm_user_assigned_identity.this.client_id
-}
-
-output "workload_identity_client_id" {
-  value = azurerm_user_assigned_identity.this.client_id
+  value = data.azurerm_user_assigned_identity.this.client_id
 }
 
 output "user_assigned_identity_principal_id" {
-  value = azurerm_user_assigned_identity.this.principal_id
+  value = data.azurerm_user_assigned_identity.this.principal_id
+}
+
+output "workload_identity_client_id" {
+  value = data.azurerm_user_assigned_identity.this.client_id
 }
 
 output "workload_identity_principal_id" {
-  value = azurerm_user_assigned_identity.this.principal_id
+  value = data.azurerm_user_assigned_identity.this.principal_id
 }
 
 output "workload_identity_service_account_name" {
