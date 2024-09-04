7dbbc06d591d3ce66536a7bdb2208b1370de04dd
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2024-09-04 11:47:36 +0200

    fix(workload-identity): Added secret for service accout name (#348)
    * added sercret for service accout name
    
    * pre-commit fixs

--- kubernetes_workload_identity_configuration/variables.tf
+++ kubernetes_workload_identity_configuration/variables.tf
@@ -1,6 +1,7 @@
 locals {
-  workload_identity_name                  = var.workload_identity_name != null ? var.workload_identity_name : "${var.workload_identity_name_prefix}-workload-identity"
-  workload_identity_client_id_secret_name = "${local.workload_identity_name}-client-id"
+  workload_identity_name                        = var.workload_identity_name != null ? var.workload_identity_name : "${var.workload_identity_name_prefix}-workload-identity"
+  workload_identity_client_id_secret_name_title = "${local.workload_identity_name}-client-id"
+  workload_identity_service_account_name_title  = "${local.workload_identity_name}-service-account-name"
 }
 
 variable "workload_identity_name_prefix" {
