c4f51441c46a59c91c52920bf402d216b81aaca0
Fabio Felici <fabio.felici@pagopa.it>
2024-10-01 09:41:27 +0200

    Initial commit for kubernetes events exporter

---
+++ kubernetes_event_exporter/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.30"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
