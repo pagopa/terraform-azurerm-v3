27f15c3567fa63fc77b1882027d72cc476420cb4
Fabio Felici <fabio.felici@pagopa.it>
2024-12-24 10:18:01 +0100

    Update Helm provider version constraint in Terraform
    Changed the Helm provider version from "~> 2.0.0" to ">= 2.0.0" to allow for greater flexibility with newer versions. This ensures compatibility with future updates while maintaining the minimum required version.

--- kubernetes_opencosts/99_versions.tf
+++ kubernetes_opencosts/99_versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0.0"
+      version = ">= 2.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
