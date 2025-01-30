30ee53689a51434cf6db181efa8027a6c98b8ce6
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-01-30 11:36:22 +0100

    removed variables not more used

--- kubernetes_prometheus_install/variables.tf
+++ kubernetes_prometheus_install/variables.tf
@@ -12,30 +12,6 @@ variable "storage_class_name" {
 variable "prometheus_helm" {
   type = object({
     chart_version = optional(string, "25.24.1")
-    server = object({
-      image_name = optional(string, "quay.io/prometheus/prometheus"),
-      image_tag  = optional(string, "v2.53.1"),
-    }),
-    alertmanager = object({
-      image_name = optional(string, "quay.io/prometheus/alertmanager"),
-      image_tag  = optional(string, "v0.27.0"),
-    }),
-    node_exporter = object({
-      image_name = optional(string, "quay.io/prometheus/node-exporter"),
-      image_tag  = optional(string, "v1.8.2"),
-    }),
-    configmap_reload_prometheus = object({
-      image_name = optional(string, "jimmidyson/configmap-reload"),
-      image_tag  = optional(string, "v0.13.1"),
-    }),
-    configmap_reload_alertmanager = object({
-      image_name = optional(string, "jimmidyson/configmap-reload"),
-      image_tag  = optional(string, "v0.13.1"),
-    }),
-    pushgateway = object({
-      image_name = optional(string, "prom/pushgateway"),
-      image_tag  = optional(string, "v1.9.0"),
-    }),
   })
 
   description = "Prometheus helm chart configuration"
@@ -43,30 +19,6 @@ variable "prometheus_helm" {
 
   default = {
     chart_version = "25.24.1"
-    server = {
-      image_name = "quay.io/prometheus/prometheus"
-      image_tag  = "v2.53.1",
-    }
-    alertmanager = {
-      image_name = "quay.io/prometheus/alertmanager"
-      image_tag  = "v0.27.0",
-    }
-    node_exporter = {
-      image_name = "quay.io/prometheus/node-exporter"
-      image_tag  = "v1.8.2"
-    }
-    configmap_reload_prometheus = {
-      image_name = "jimmidyson/configmap-reload"
-      image_tag  = "v0.13.1"
-    }
-    configmap_reload_alertmanager = {
-      image_name = "jimmidyson/configmap-reload"
-      image_tag  = "v0.13.1"
-    }
-    pushgateway = {
-      image_name = "prom/pushgateway"
-      image_tag  = "v1.9.0"
-    }
   }
 }
 
