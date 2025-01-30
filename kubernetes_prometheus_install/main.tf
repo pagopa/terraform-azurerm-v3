1f045abec12b9fed90cea008bdfc13e2455190c9
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-01-30 11:34:50 +0100

    now installation use values file to simplify the installation

--- kubernetes_prometheus_install/main.tf
+++ kubernetes_prometheus_install/main.tf
@@ -8,82 +8,19 @@ resource "null_resource" "trigger_helm_release" {
 }
 
 resource "helm_release" "prometheus" {
-
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
   version    = var.prometheus_helm.chart_version
   namespace  = var.prometheus_namespace
 
-  set {
-    name  = "server.global.scrape_interval"
-    value = "30s"
-  }
-  set {
-    name  = "alertmanager.image.repository"
-    value = var.prometheus_helm.alertmanager.image_name
-  }
-  set {
-    name  = "alertmanager.image.tag"
-    value = var.prometheus_helm.alertmanager.image_tag
-  }
-  set {
-    name  = "alertmanager.configmapReload.prometheus.image.repository"
-    value = var.prometheus_helm.configmap_reload_prometheus.image_name
-  }
-  set {
-    name  = "alertmanager.configmapReload.prometheus.image.tag"
-    value = var.prometheus_helm.configmap_reload_prometheus.image_tag
-  }
-  set {
-    name  = "alertmanager.configmapReload.alertmanager.image.repository"
-    value = var.prometheus_helm.configmap_reload_alertmanager.image_name
-  }
-  set {
-    name  = "alertmanager.configmapReload.alertmanager.image.tag"
-    value = var.prometheus_helm.configmap_reload_alertmanager.image_tag
-  }
-  set {
-    name  = "alertmanager.nodeExporter.image.repository"
-    value = var.prometheus_helm.node_exporter.image_name
-  }
-  set {
-    name  = "alertmanager.nodeExporter.image.tag"
-    value = var.prometheus_helm.node_exporter.image_tag
-  }
-  set {
-    name  = "alertmanager.nodeExporter.image.repository"
-    value = var.prometheus_helm.node_exporter.image_name
-  }
-  set {
-    name  = "alertmanager.nodeExporter.image.tag"
-    value = var.prometheus_helm.node_exporter.image_tag
-  }
-  set {
-    name  = "alertmanager.server.image.repository"
-    value = var.prometheus_helm.server.image_name
-  }
-  set {
-    name  = "alertmanager.server.image.tag"
-    value = var.prometheus_helm.server.image_tag
-  }
-  set {
-    name  = "alertmanager.pushgateway.image.repository"
-    value = var.prometheus_helm.pushgateway.image_name
-  }
-  set {
-    name  = "alertmanager.pushgateway.image.tag"
-    value = var.prometheus_helm.pushgateway.image_tag
-  }
-  set {
-    name  = "alertmanager.persistentVolume.storageClass"
-    value = var.storage_class_name
-  }
-  set {
-    name  = "server.persistentVolume.storageClass"
-    value = var.storage_class_name
-  }
+  values = [
+    templatefile("${path.module}/values.yaml", {
+    storage_class_name = var.storage_class_name
+  })
+  ]
 
+  # Lifecycle policy to handle updates
   lifecycle {
     replace_triggered_by = [
       null_resource.trigger_helm_release
