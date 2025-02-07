f8e4e1d3a6df3479c31f80c1732adcb357183895
Fabio Felici <fabio.felici@pagopa.it>
2025-02-07 12:51:38 +0100

    Add Prometheus alert rule for node CPU usage monitoring
    Introduce a new Prometheus alert rule group to monitor node CPU utilization in Kubernetes clusters. The rule triggers a warning when CPU usage exceeds 80% for more than 10 minutes, providing helpful labels and annotations for better visibility.

---
+++ kubernetes_prometheus_managed/02_prometheus_alerts.tf
@@ -0,0 +1,25 @@
+resource "azurerm_monitor_alert_prometheus_rule_group" "node_recording_rules_alert_group" {
+  name                = "NodeRecordingRulesRuleGroup-Alerts-${var.cluster_name}"
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+  cluster_name        = var.cluster_name
+  description         = "Node Recording Alerts Rule Group"
+  rule_group_enabled  = true
+  interval            = "PT1M"
+  scopes = [
+    azurerm_monitor_workspace.this.id, data.azurerm_kubernetes_cluster.this.id
+  ]
+  tags = var.tags
+
+  rule {
+    expression = "avg by (instance) (rate(node_cpu_seconds_total{mode!='idle'}[5m])) > 0.08"
+    for        = "15m"
+    labels = {
+      severity = "warning"
+    }
+    annotations = {
+      summary     = "Elevato utilizzo della CPU del nodo rilevato"
+      description = "L'utilizzo medio della CPU per il nodo {{ $labels.instance }} ha superato l'80% per più di 10 minuti."
+    }
+  }
+}
