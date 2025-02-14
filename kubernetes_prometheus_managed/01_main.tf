7a522c9076a114a1addf4ff4b3474786dc65bf8b
Fabio Felici <fabio.felici@pagopa.it>
2025-02-14 11:50:05 +0100

    Update private endpoint resource type in Terraform
    Changed the resource type for the private endpoint connection from `Microsoft.Dashboard/grafana` to `Microsoft.Network/privateEndpoints`. This ensures compatibility with the correct Azure resource hierarchy and resolves potential deployment issues.

--- kubernetes_prometheus_managed/01_main.tf
+++ kubernetes_prometheus_managed/01_main.tf
@@ -134,7 +134,7 @@ resource "azapi_resource" "grafana_managed_private_endpoint_ma" {
 
 # # Approve the private endpoint
 resource "azapi_update_resource" "approval" {
-  type      = "Microsoft.Dashboard/grafana/privateEndpointConnections@2023-10-01-preview"
+  type      = "Microsoft.Network/privateEndpoints/privateEndpointConnections@2023-10-01-preview"
   name      = "grafana-${data.azurerm_dashboard_grafana.grafana.name}-pagopa${var.tags["Environment"]}${var.location_short}GrafPam"
   parent_id = azapi_resource.grafana_managed_private_endpoint_ma.body.properties.privateLinkResourceId
 
