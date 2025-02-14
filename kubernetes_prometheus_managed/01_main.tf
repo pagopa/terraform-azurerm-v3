da77a6edf7ab6fd8fe1bc3b2b68a43889296a787
Fabio Felici <fabio.felici@pagopa.it>
2025-02-14 10:54:18 +0100

    Rename Grafana resource for consistency.
    Updated the `azapi_resource` name to streamline and align with naming conventions. This change improves readability and ensures uniformity across resource definitions.

--- kubernetes_prometheus_managed/01_main.tf
+++ kubernetes_prometheus_managed/01_main.tf
@@ -110,7 +110,7 @@ resource "azurerm_role_assignment" "datareaderrole" {
 # https://registry.terraform.io/providers/hashicorp/azurerm/4.9.0/docs/resources/dashboard_grafana_managed_private_endpoint
 resource "azapi_resource" "grafana_managed_private_endpoint_ma" {
   type      = "Microsoft.Dashboard/grafana/managedPrivateEndpoints@2023-10-01-preview"
-  name      = "pagopa${var.tags["Environment"]}${var.location_short}GrafanaPam"
+  name      = "pagopa${var.tags["Environment"]}${var.location_short}GrafPam"
   location  = var.location
   tags      = var.tags
   parent_id = data.azurerm_dashboard_grafana.grafana.id
