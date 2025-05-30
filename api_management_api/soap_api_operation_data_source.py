b6c7f806b7738488f33998a1e2eee4f975d1bbff
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-05-30 10:27:53 +0200

    wip

--- api_management_api/soap_api_operation_data_source.py
+++ api_management_api/soap_api_operation_data_source.py
@@ -6,14 +6,20 @@ import subprocess
 def find_operation_ids(api_id: str, service_name: str, resource_group: str) -> str:
   # operations = subprocess.run(f'az apim api operation list --resource-group {resource_group} --service-name {service_name} --api-id {api_id} --query "[].{id:id, name:name, urlTemplate:urlTemplate}" --output json')
   operations = subprocess.run(['az', 'apim', 'api', 'operation', 'list','--resource-group', resource_group, '--service-name', service_name, '--api-id', api_id, '--query', '[].{id:id, name:name, urlTemplate:urlTemplate}', '--output', 'json'], capture_output=True, text=True)
-  return operations
+  operations_j = json.loads(operations)
+
+  result = {}
+
+  for operation in operations_j:
+    result[operation['name']] = operation['urlTemplate']
+  return result
 
 def main(query):
   rg = query['resource_group']
   service_name = query['service_name']
   api_id = query['api_id']
   operation_ids = find_operation_ids(api_id, service_name, rg)
-  print(operation_ids)
+  print(json.dumps(operation_ids))
 
 
 if __name__ == "__main__":
